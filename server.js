5dcc42fad4bcdea13616a6c49520fa038578ed69
Greytiger23 <104874165+Greytiger23@users.noreply.github.com>
2024-09-18 21:42:21 +0200

    server file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json())
+
+app.use('/api/posts', require('./routes/posts'));
+
+
+mongoose.connect(Pr);
