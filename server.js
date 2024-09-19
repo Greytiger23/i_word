e3f18bed09b79223f3da52a94a3e91f96b4d8e8d
Greytiger23 <isabelmothoa49@gmail.com>
2024-09-18 20:30:55 -0700

    packages and testing

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const auth = require('../middleware/auth');
-app.use('/api/posts', auth, require('./routes/posts'));
+const auth = require('./src/middleware/auth');
+app.use('/api/posts', auth, require('./src/routes/posts'));
 
 
-mongoose.connect(process.env.MONGo_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected'))
